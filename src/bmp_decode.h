3017933dd70583306f1570773a7064d9d5f46d3e
tantanGH <tantangh68@gmail.com>
2023-03-23 22:29:33 +0900

    initial commit

---
+++ src/bmp_decode.h
@@ -0,0 +1,21 @@
+#ifndef __H_BMP_DECODE__
+#define __H_BMP_DECODE__
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+
+typedef struct {
+  int16_t brightness;
+  int16_t half_size;
+  int16_t extended_graphic;
+  uint16_t* rgb555_r;
+  uint16_t* rgb555_g;
+  uint16_t* rgb555_b;
+} BMP_DECODE_HANDLE;
+
+int32_t bmp_decode_init(BMP_DECODE_HANDLE* bmp, int16_t brightness, int16_t half_size, int16_t extended_graphic);
+void bmp_decode_close(BMP_DECODE_HANDLE* bmp);
+int32_t bmp_decode_exec(BMP_DECODE_HANDLE* bmp, FILE* fp);
+
+#endif
